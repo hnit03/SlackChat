c554384edbb4767f2f1754b48cbdb6283e082ed8
hnit03 <hoangnhinguyen33@gmail.com>
2021-04-11 11:18:05 +0700

    update app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import {Grid} from 'semantic-ui-react';
 import "./App.css";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+import SidePanel from "./SidePanel/SidePanel";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return(
+      <Grid columns="equal" className="app">
+      <ColorPanel />
+      <SidePanel />
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages/>
+      </Grid.Column>
+
+      <Grid.Column width="4">
+        <MetaPanel/>
+      </Grid.Column>
+      
+    </Grid>
+    );
   }
 }
 
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SidePanel extends Component{
+    render(){
+        return(
+            <div>MetaPanel</div>
+        );
+    }
+}
+
+export default SidePanel;
