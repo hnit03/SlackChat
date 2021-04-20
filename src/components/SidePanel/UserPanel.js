09f94d810b41e84f94faa1d5df725a0422b30510
hnit03 <hoangnhinguyen33@gmail.com>
2021-04-20 22:10:53 +0700

    crop image with avatar editor

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import firebase from "../../firebase";
+import AvatarEditor from 'react-avatar-editor';
 import { Button, Dropdown, Grid, Header, Icon,Image,Input,Modal } from "semantic-ui-react";
 
 class UserPanel extends Component{
 
     state = {
         user : this.props.currentUser,
-        modal:false
+        modal:false,
+        previewImage: '',
+        croppedImage:'',
+        blob:''
     }
 
     openModal = () => this.setState({ modal: true });
@@ -38,9 +42,32 @@ class UserPanel extends Component{
         .signOut();
     };
 
+    handleChange = event =>{
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        if (file) {
+            reader.readAsDataURL(file);
+            reader.addEventListener('load',()=>{
+                this.setState({ previewImage: reader.result });
+            });
+        }
+    }
+
+    handleCropImage = () =>{
+        if (this.avatarEditor) {
+            this.avatarEditor.getImageScaledToCanvas().toBlob(blob =>{
+                let imageURL = URL.createObjectURL(blob);
+                this.setState({
+                    croppedImage: imageURL
+                });
+            })
+        }
+    }
+
     render(){
 
-        const { user,modal } = this.state;
+        const { user,modal,previewImage,croppedImage } = this.state;
         return(
             <Grid style={{background:'#3498db'}}>
                 <Grid.Column>
@@ -68,23 +95,40 @@ class UserPanel extends Component{
                                 type='file'
                                 label='New Avatar'
                                 name='previewImage'
+                                onChange={this.handleChange}
                             />
                             <Grid centered stackable columns={2} >
                                 <Grid.Row centered >
                                     <Grid.Column className='ui center aligned grid'>
-                                    
+                                        {previewImage && (
+                                            <AvatarEditor 
+                                                ref= {node => (this.avatarEditor = node)}
+                                                image={previewImage}
+                                                width={200}
+                                                height={200}
+                                                border={50}
+                                                scale={1.2}
+                                            />
+                                        )}
                                     </Grid.Column>
                                     <Grid.Column>
-                                    
+                                        {croppedImage &&(
+                                            <Image 
+                                                src={croppedImage}
+                                                style={{ margin:'3.5em auto' }}
+                                                width={150}
+                                                height={150}
+                                            />
+                                        )}
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
                         </Modal.Content>
                         <Modal.Actions>
-                            <Button color='green' inverted> 
+                            {croppedImage && <Button color='green' inverted> 
                                 <Icon name='save'/> Change Avatar
-                            </Button>
-                            <Button color='green' inverted> 
+                            </Button>}
+                            <Button color='green' inverted onClick={this.handleCropImage}> 
                                 <Icon name='image'/> Preview
                             </Button>
                             <Button color='red' inverted onClick={this.closeModal}> 
